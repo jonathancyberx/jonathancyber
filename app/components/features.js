c6e5572cae61292ed921982090e2f36d0248ad90
Camelia <Cameliaafolabi@gmail.com>
2025-02-19 21:10:47 +0100

    modified pictures for the feature section

--- app/components/features.js
+++ app/components/features.js
@@ -13,40 +13,40 @@ import feature9 from "../../public/features-9.gif"
 
 const Features = () => {
   return (
-    <section className="bg-[#122455]  lg:w-4/5 rounded-xl m-auto py-16">
+    <section className="bg-[#122455]  lg:w-4/5 rounded-xl m-auto py-6 lg:py-16">
       <div className="max-w-6xl mx-auto px-6">
         <h2 className="text-4xl text-center text-white mb-8 font-bold">Features</h2>
         <div className="overflow-hidden relative">
           <div className="flex flex-wrap lg:space-x-10  gap-4 lg:justify-between justify-evenly items-center">
-            <div className="flex-shrink-0">
-              <Image src={feature1} alt="Logo 1" width={ 50 } height={ 50 } />
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
+              <Image src={feature1} alt="Logo 1" width={ 50 } height={ 50 }  />
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature2} alt="Logo 2" width={ 50 } height={ 50 } />
 
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature3} alt="Logo 3" width={ 50 } height={ 50 } />
 
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature4} alt="Logo 4" width={ 50 } height={ 50 } />
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature5} alt="Logo 5" width={ 50 } height={ 50 } />
             </div>
             {/* Duplicate logos */}
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature6} alt="Logo 1" width={ 50 } height={ 50 } />
 
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature7} alt="Logo 2" width={ 50 } height={ 50 } />
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature8} alt="Logo 3" width={ 50 } height={ 50 } />
             </div>
-            <div className="flex-shrink-0">
+            <div className="w-1/4 md:w-1/6 lg:w-1/12">
               <Image src={feature9} alt="Logo 4" width={ 50 } height={ 50 } />
             </div>
            
